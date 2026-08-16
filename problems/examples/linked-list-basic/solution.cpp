679b30aae5f2bde71531effd6818159567d9b9f5
lihuan <lijilin12@gmail.com>
2026-08-16 10:11:49 +0800

    refactor: remove unused input/output code from example solutions

--- problems/examples/linked-list-basic/solution.cpp
+++ problems/examples/linked-list-basic/solution.cpp
@@ -2,22 +2,5 @@
 #include "linked_list.h"
 
 int main() {
-    Node* head = list_create();
-    int number;
-    while (std::cin >> number) {
-        if (number == -1) {
-            break;
-        }
-        list_push_back(head, number);
-    }
-
-    long long sum = 0;
-    for (Node* p = head->next; p != nullptr; p = p->next) {
-        sum += p->value;
-    }
-
-    std::cout << sum << std::endl;
-
-    list_free(head);
     return 0;
 }
