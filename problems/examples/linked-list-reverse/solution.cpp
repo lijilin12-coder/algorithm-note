29fa88ce9609ff27d76e802641a57b746e44b9cc
huan.li <huanli0902@gmail.com>
2026-08-28 11:39:02 +0800

    git commit 提交作业

--- problems/examples/linked-list-reverse/solution.cpp
+++ problems/examples/linked-list-reverse/solution.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
 #include "linked_list.h"
 
-Node* reverse_list(Node* head) {
-    Node* previous = NULL;
-    Node* current = head;
+Node *reverse_list(Node *head)
+{
+    Node *p = head;
+    Node *q = head->next;
+    Node *newhead = (Node *)malloc(sizeof(Node));
+    newhead->value = -1;
+    newhead->next = nullptr;
 
-    while (current != NULL) {
-        Node* next = current->next;
-        current->next = previous;
-        previous = current;
-        current = next;
+    while (p != NULL)
+    {
+        q = p->next;
+        p->next = newhead->next;
+        newhead->next = p;
+        p = q;
     }
-
-    return previous;
+    return newhead->next;
 }
 
-int main() {
-    Node* sentinel = list_create();
+int main()
+{
+    Node *sentinel = list_create();
     int value;
 
-    while (std::cin >> value && value != -1) {
+    while (std::cin >> value && value != -1)
+    {
         list_push_back(sentinel, value);
     }
 
     sentinel->next = reverse_list(sentinel->next);
 
-    Node* current = sentinel->next;
-    while (current != NULL) {
-        if (current != sentinel->next) {
+    Node *current = sentinel->next;
+    while (current != NULL)
+    {
+        if (current != sentinel->next)
+        {
             std::cout << ' ';
         }
         std::cout << current->value;
