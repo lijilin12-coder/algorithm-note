2e939c1bec9cdc623a0b627e492742331717d2fb
jilin.li <lijilin12@gmail.com>
2026-09-10 00:54:19 -0600

    前缀和

---
+++ problems/3array/1matrix-block-sum/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+        // 在这里实现你的代码
+        return {};
+    }
+};
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    vector<vector<int>> mat(m, vector<int>(n));
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            cin >> mat[i][j];
+
+    int k;
+    cin >> k;
+
+    Solution sol;
+    vector<vector<int>> ans = sol.matrixBlockSum(mat, k);
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (j) cout << ' ';
+            cout << ans[i][j];
+        }
+        cout << '\n';
+    }
+    return 0;
+}
