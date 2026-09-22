117d772a5f81bd781714100aa43d7bdee357bbb6
jilin.li <lijilin12@gmail.com>
2026-09-21 20:37:48 -0600

    不同的子序列

--- problems/4dynamic-programming/3russian-doll-envelopes/solution.cpp
+++ problems/4dynamic-programming/3russian-doll-envelopes/solution.cpp
@@ -6,48 +6,9 @@ using namespace std;
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        int n = envelopes.size();
-        sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] == b[0] ? b[1] < a[1] : a[0] < b[0];
-        });
-
-        // 对高度数组寻找LIS
-        vector<int> hight;
-        for(auto& item : envelopes) {
-            hight.push_back(item[1]);
-        }
-        return lengthOfLIS(hight);
+        
     }
 
-    int lengthOfLIS(vector<int>& nums) 
-    {
-        vector<int> top(nums.size());
-        // 牌堆数初始化为 0
-        int piles = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            // 要处理的扑克牌
-            int poker = nums[i];
-
-            // ***** 搜索左侧边界的二分查找 *****
-            int left = 0, right = piles-1;
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                if (top[mid] >= poker) {
-                    right = mid-1;
-                } else if (top[mid] < poker) {
-                    left = mid + 1;
-                }
-            }
-            // ********************************
-            
-            // 没找到合适的牌堆，新建一堆
-            if (left == piles) piles++;
-            // 把这张牌放到牌堆顶
-            top[left] = poker;
-        }
-        // 牌堆数就是 LIS 长度
-        return piles;
-    }
 };
 
 int main() {
---
+++ problems/4dynamic-programming/4distinct-subsequences/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+
+    }
+
+};
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    Solution sol;
+    cout << sol.numDistinct(s, t) << '\n';
+    return 0;
+}
